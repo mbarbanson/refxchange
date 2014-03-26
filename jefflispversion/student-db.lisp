db31224ae9aaa4cf105f1e89346f43e165f4cdc3
Jeff Shrager <jshrager@stanford.edu>
2014-03-26 14:10:15 -0700

    no message

---
+++ jefflispversion/student-db.lisp
@@ -0,0 +1,14 @@
+(
+ :id 12345
+ :full-name "Jane Doe"
+ :brief-name "Jane"
+ :gpa 3.5
+ :ecas ("Girls volleyball" "Math club")
+ )
+(
+ :id 31415
+ :full-name "John Jones"
+ :brief-name "JJ"
+ :gpa 3.3
+ :ecas ("Rocketry club" "Knitting club")
+ )
